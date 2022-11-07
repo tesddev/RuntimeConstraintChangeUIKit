6da6aea162365be2a36cb563036f8dd82e6a50af
tesddev <tesleem.amuda@gmail.com>
2022-11-07 11:17:06 +0100

    add file

---
+++ RuntimeConstraintChangeUIKit.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension UIView {
+    func setHeight(_ h:CGFloat, animateTime:TimeInterval?=nil) {
+
+        if let c = self.constraints.first(where: { $0.firstAttribute == .height && $0.relation == .equal }) {
+            c.constant = CGFloat(h)
+
+            if let animateTime = animateTime {
+                UIView.animate(withDuration: animateTime, animations:{
+                    self.superview?.layoutIfNeeded()
+                })
+            }
+            else {
+                self.superview?.layoutIfNeeded()
+            }
+        }
+    }
+}
